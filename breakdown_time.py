c3de550e671f0e6e5215882989b6379f2878dceb
Alisa <alicevolchenok@gmail.com>
2025-11-10 21:10:08 +0100

    homework is done

--- breakdown_time.py
+++ breakdown_time.py
@@ -1,4 +1,26 @@
 def breakdown_time(seconds):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    
+    if type(seconds) != int or seconds < 0:
+        return -1
+    if seconds == 0:
+        return {}
+
+    hours =0
+    minutes = 0
+
+    while seconds >= 3600:
+        hours += 1
+        seconds -= 3600
+    
+    while seconds >= 60:
+        minutes += 1
+        seconds -= 60
+
+
+    if minutes == 0:
+        return {3600: hours}
+    
+
+    return {3600: hours, 60: minutes, 1: seconds}
+
+
--- swap_ends.py
+++ swap_ends.py
@@ -1,4 +1,12 @@
 def swap_ends(L, k):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    #use suffix and preffi
+    if k <= 0 or k > len(L) // 2:
+        copy_L = L[:]
+        return (copy_L, 0)
+    
+    c = L[:k]
+    b = L[k:len(L) - k]
+    a = L[-k:]
+
+    return (a + b + c, k)
+    
